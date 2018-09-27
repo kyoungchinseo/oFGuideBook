024b5af5b38cd3f42741329149d2c61df76e6e46
Kyoungchin Seo <kcseo.acm@gmail.com>
2018-09-27 23:14:12 +0900

    fix movingball class

--- chapter06/myMoveBall/myMoveBalls_03_draw_balls_trails/src/MovingBall.h
+++ chapter06/myMoveBall/myMoveBalls_03_draw_balls_trails/src/MovingBall.h
@@ -17,14 +17,11 @@ public:
     ~MovingBall();
     
     void setup(ofVec2f l, ofVec2f v, ofVec2f a, ofColor c, float r);
-    void reset();
     void update();
     void draw();
     void drawTrail();
     void checkBoundary();
-    void checkPass();
     void applyForce(ofVec2f force);
-    void applyAttraction(ofVec2f force, ofVec2f pos);
     
 public:
     ofVec2f location;
